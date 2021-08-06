3cb6e894c9068fece50170b52c87124a43247eac
kaivon tolooee <kaivont@gmail.com>
2021-08-06 18:37:36 -0400

    fixing the database.js spelling issues

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-Let  MONGODB_URI=process.env.MONGODB_URI || "mongodb://localhost/budget";
-
-mongoose.connect(MONGODB_URI, {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
