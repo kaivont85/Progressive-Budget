b8d31fa7ccc1055faae8086df3a92877d3ced30b
kaivon tolooee <kaivont@gmail.com>
2021-08-06 18:27:26 -0400

    fixing my serviceWorker.js and adding it my index.html

--- public/index.js
+++ public/index.js
@@ -78,6 +78,8 @@ function populateChart() {
   });
 }
 
+
+
 function sendTransaction(isAdding) {
   let nameEl = document.querySelector("#t-name");
   let amountEl = document.querySelector("#t-amount");
