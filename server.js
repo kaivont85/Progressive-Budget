f52783bcc586542b9e28f04497f1632f3a791039
kaivon tolooee <kaivont@gmail.com>
2021-08-06 20:44:47 -0400

    trying to fix heroku errors

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Progressive-Budget", {
+let MONGODB_URI=process.env.MONGODB_URI || "mongodb://localhost/Progressive-Budget";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}
-);
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
